e975bda051639e535f32cc2e55ce1219a6d79ba8
bahp <bernard.hernandez.perez@gmail.com>
2024-05-24 18:39:32 +0100

    ~ pwa

--- sw.js
+++ sw.js
@@ -1,12 +1,23 @@
-var cacheName = 'hello-pwa';
+var cacheName = 'timerhub';
 var filesToCache = [
   '/',
   '/index.html',
+  '/index-v2.html',
   '/css/style.css',
+  '/css/pdx.css',
+  '/css/main.css',
+  '/css/bootstrap.min.css',
   '/js/main.js',
+  '/js/bootstrap-5.3.0.min.js',
+  '/js/dexie-3.2.4.min.js',
+  '/js/easytimer-1.1.3.min.js',
+  '/js/jquery-3.6.0.min.js',
+  '/js/moment-2.29.1.min.js',
   '/audio/mixkit-clear-announce-tones-2861.wav',
   '/audio/mixkit-happy-bells-notification-937.wav',
-  '/audio/mixkit-urgent-simple-tone-loop-2976.wav'
+  '/audio/mixkit-urgent-simple-tone-loop-2976.wav',
+  '/images/logo-pdx.svg',
+  '/images/logo-pdx.png'
 ];
 
 /* Start the service worker and cache all of the app's content */
