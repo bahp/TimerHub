731b0310b90e275ce55f388f323e162aa62d8e1b
Imperial College <cbit@Imperials-MBP-2.cust.communityfibre.co.uk>
2024-05-25 16:59:18 +0100

    ~ pwa sampe

--- tests/pwa/sw.js
+++ tests/pwa/sw.js
@@ -1,7 +1,45 @@
+/*const staticDevCoffee = "dev-coffee-site-v1"
+const assets = [
+  "/",
+  "/index.html",
+  "/css/style.css",
+  "/js/app.js",
+  "/images/coffee1.jpg",
+  "/images/coffee2.jpg",
+  "/images/coffee3.jpg",
+  "/images/coffee4.jpg",
+  "/images/coffee5.jpg",
+  "/images/coffee6.jpg",
+  "/images/coffee7.jpg",
+  "/images/coffee8.jpg",
+  "/images/coffee9.jpg",
+]
+
+self.addEventListener("install", installEvent => {
+  installEvent.waitUntil(
+    caches.open(staticDevCoffee).then(cache => {
+      cache.addAll(assets)
+    })
+  )
+})
+
+
+self.addEventListener("fetch", fetchEvent => {
+  fetchEvent.respondWith(
+    caches.match(fetchEvent.request).then(res => {
+      return res || fetch(fetchEvent.request)
+    })
+  )
+})
+*/
+
+
+
 var cacheName = 'timerhub';
 var filesToCache = [
   '/',
   '/pwa.html',
+  '/logo-pdx.png'
 ];
 
 /* Start the service worker and cache all of the app's content */
